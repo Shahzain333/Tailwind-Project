51747c8efe35595672061db9069276d559a5882f
Shahzain khan <108424969+Shahzain333@users.noreply.github.com>
2024-09-11 11:43:43 +0500

    Update script.js

--- Project/script.js
+++ Project/script.js
@@ -1,68 +1,78 @@
-const navDialog = document.getElementById('nav-dialog'); 
+const downloadCV = document.getElementById('downloadCV');
+const Contact = document.getElementById('Contact');
+const linkedin = document.getElementById('linkedin');
+const github = document.querySelector('#github');
+const mail = document.querySelector('#mail');
+const instgram = document.getElementById('instgram');
+const textMotion = document.getElementById('text_motion');
 
-function handleMenu(){
-    navDialog.classList.toggle('hidden');
+/* =================== Text iterable ============== */
+
+const text = [
+    "Fronted Developer",
+    "FullStack Developer",
+    "Web Developer"
+] 
+
+let speed = 100;
+
+let textIndex = 0;
+let characterIndex = 0;
+
+window.onload = typeWriter();
+
+function typeWriter() {
+
+    if(characterIndex < text[textIndex].length){
+        textMotion.innerHTML += text[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typeWriter,speed);
+    }else {
+        setTimeout(eraseText,1000);
+    }
 }
 
-
-const initialTranslateLTR = -48*4;
-const initialTranslateRTL = 36*4;
-
-function setupIntersectionObserver(element, isLTR, speed) {
-
-    const intersectionCallback = (entries) => {
-    
-        const isIntersecting = entries[0].isIntersecting;
-    
-        if(isIntersecting) {
-            document.addEventListener('scroll', scrollHandler);
-        } else {
-            document.removeEventListener('scroll', scrollHandler);
-        }
-    
+function eraseText() {
+    if(textMotion.innerHTML.length > 0){
+        textMotion.innerHTML = textMotion.innerHTML.slice(0,-1);
+        setTimeout(eraseText,50);
+    }else {
+        textIndex = (textIndex + 1) % text.length;
+        characterIndex = 0;
+        setTimeout(typeWriter,500);
     }
-    
-    const intersectionObserver = new IntersectionObserver(intersectionCallback);
-
-    intersectionObserver.observe(element);
-
-    function scrollHandler() {
-    
-        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
-
-        let totalTranslate = 0;
-    
-        if(isLTR) {
-            totalTranslate = translateX + initialTranslateLTR;
-        } else {
-            totalTranslate = -(translateX + initialTranslateRTL);
-        }
-
-        element.style.transform = `translateX(${totalTranslate}px)`;
-    
-    }
-
 }
 
-const line1 = document.getElementById('line1');
-const line2 = document.getElementById('line2');
-const line3 = document.getElementById('line3');
+/* ========================= Click events of buttons & Icon ============================== */
 
-setupIntersectionObserver(line1, true, 0.15);
-setupIntersectionObserver(line2, false, 0.15);
-setupIntersectionObserver(line3, true, 0.15);
+downloadCV.addEventListener('click', () => {
+    window.open('./assets/Shahzain khan.pdf');
+});
 
+Contact.addEventListener('click', () => {
+    location.href = "#contact"
+});
 
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/shahzainkhan/')
+});
 
+github.addEventListener('click', () => {
+    window.open('https://github.com/Shahzain333')
+});
 
+instgram.addEventListener('click', () => {
+    window.open('https://www.instagram.com/shahzain._.niazi/profilecard/?igsh=MXNieTBkYzl6Mmd4Ng==')
+})
 
+mail.addEventListener('click', () => {
+    window.open('mailto:shahzainkhannaizi1234@gmail.com')
+})
 
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".mobile-icon");
 
-
-
-
-
-
-
-
-
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
